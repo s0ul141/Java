bcfe3bb1b9ba16dc3eb961c87c2e0a098845f976
Soumya_Basu <basusoumya366@gmail.com>
2023-02-10 00:52:53 +0530

    hm_qsp_10/2

---
+++ uni.java
@@ -0,0 +1,14 @@
+class uni
+{
+	public static void main(String[]agrs)
+	{
+		double exam_cost = 2000.00;
+		String course_name = "BSc.in IT(DS)";
+		String unvir_name = "MAKAUT";
+		System.out.println("Course name "+course_name);
+		System.out.println("University name "+unvir_name);
+		System.out.println("exam cost "+exam_cost);	
+	}
+
+
+}
