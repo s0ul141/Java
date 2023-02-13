6756828764addcb66af1156d3b573846a8022d24
Soumya_Basu <basusoumya366@gmail.com>
2023-02-13 22:49:42 +0530

    methods

---
+++ methods/m1.java
@@ -0,0 +1,15 @@
+class m1
+{
+	static void add()
+		{
+		int a = 6, b=7;
+		int c = a+b;
+		System.out.println(c);
+		}
+	public static void main (String[]args)
+	{
+		System.out.println("***main starts***");
+		add();
+		System.out.println("***main ends***");
+	}
+}
---
+++ methods/m2.java
@@ -0,0 +1,15 @@
+class m3
+{
+	static void add3()
+		{
+		int a = 6, b=7, d=89;
+		int c = a+b+d;
+		System.out.println(c);
+		}
+	public static void main (String[]args)
+	{
+		System.out.println("***main starts***");
+		add3();
+		System.out.println("***main ends***");
+	}
+}
