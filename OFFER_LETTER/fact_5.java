51da376d96187497d877d8b8a5b5a7a28a1eaf86
Soumya_Basu <basusoumya366@gmail.com>
2023-02-17 09:51:43 +0530

    logical_prog

---
+++ OFFER_LETTER/fact_5.java
@@ -0,0 +1,12 @@
+class fact_5
+{
+	public static void main (String[]args)
+	{
+		int fact= 1;
+		for (int i=5; i>=1; i--)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+}
---
+++ OFFER_LETTER/fact_ass_9.java
@@ -0,0 +1,12 @@
+class fact_ass_9
+{
+	public static void main (String[]args)
+	{
+		int fact = 1;
+		for (int i =9; i>=1; i--)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+}
