96aebfb76e7115b2a0ad458b3f56c3dcb60fe9d4
Soumya_Basu <basusoumya366@gmail.com>
2023-03-27 23:12:16 +0530

    swap_without_var

---
+++ OFFER_LETTER/swap_2.java
@@ -0,0 +1,17 @@
+class swap_2
+{
+	public static void main (String[]args)
+	{
+		int a=8;
+		int b=9;
+		a=a+b;
+		
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.println(b);
+
+
+	}
+
+}
