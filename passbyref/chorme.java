6adea3d6e0a1436f8afa418dfdb49d235bb85743
Soumya_Basu <basusoumya366@gmail.com>
2023-03-29 01:13:40 +0530

    makaut

---
+++ passbyref/chorme.java
@@ -0,0 +1,23 @@
+class makaut
+{
+	void erp()
+	{
+		System.out.println("Where is 5th sem results??");
+	}
+}
+class chorme
+{
+	public static void main(String[]args)
+	{
+		makaut m1=new makaut();
+		student.sb14(m1);
+
+	}
+}
+class student
+{
+	static void sb14(makaut m2)
+	{
+		m2.erp();
+	}
+}
