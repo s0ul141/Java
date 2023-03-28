3dcac05d279d9a46d0c4dc53b8b2c0ae98aa086f
Soumya_Basu <basusoumya366@gmail.com>
2023-03-28 23:50:33 +0530

    IBM_Uncle

---
+++ passbyref/IBM_uncle.java
@@ -0,0 +1,31 @@
+class IBM_aunty
+{
+	void job ()
+	{
+		System.out.println("Software job");
+	}
+}
+class IBM_uncle
+{
+	public static void main (String[]args)
+	{
+		IBM_aunty a1 = new IBM_aunty();
+		dinga.needjob(a1);
+		dingi.needjob(a1);
+
+	}
+}
+class dinga
+{
+	static void needjob(IBM_aunty a2)
+	{
+		a2.job();
+	}
+}
+class dingi
+{
+	static void needjob(IBM_aunty a3)
+	{
+		a3.job();
+	}
+}
