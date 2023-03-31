b218cb9be810d901f5b4a836bdd43bfa6f08a6fb
Soumya_Basu <basusoumya366@gmail.com>
2023-03-31 23:47:20 +0530

    linear search

--- arry/linear.java
+++ arry/linear.java
@@ -4,12 +4,20 @@ class linear
 	{
 		int [] arr={5,10,15,20,25};
 		int find = 15;
+		int c=0;
 		for (int i=0; i< arr.length; i++)
 		{
 			if (arr[i]==find)
-			{
+			{	c=1;
 				System.out.println("found");
 			}
+			
 		}
+		if (c==0)
+		{
+			System.out.println("not found");
+		}
+		
 	}
+
 }
