4039e8d3088bb4f428430a79387262add6383368
Soumya_Basu <basusoumya366@gmail.com>
2023-03-31 23:35:45 +0530

    evenofarray

---
+++ arry/evenar.java
@@ -0,0 +1,15 @@
+class evenar
+{
+	public static void main(String[]args)
+	{
+		int[]arr={5,10,15,20,25};
+		for (int i=0; i<arr.length; i++)
+		{
+			if (arr[i]%2==0)
+			{	
+				System.out.println(arr[i]);	
+			}
+			
+		}
+	}
+}
